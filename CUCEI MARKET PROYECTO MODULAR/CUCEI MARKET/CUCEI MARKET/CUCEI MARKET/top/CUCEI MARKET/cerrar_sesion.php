e7325156fe3e16e747b074014a8456b678fba487
Paulina-Martinez-Villarreal <85911997+Paulina-Martinez-Villarreal@users.noreply.github.com>
2025-03-25 23:13:48 -0600

    Delete CUCEI MARKET PROYECTO MODULAR/CUCEI MARKET/CUCEI MARKET/CUCEI MARKET/top/CUCEI MARKET/cerrar_sesion.php

--- CUCEI MARKET PROYECTO MODULAR/CUCEI MARKET/CUCEI MARKET/CUCEI MARKET/top/CUCEI MARKET/cerrar_sesion.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();  // Inicia la sesión
-session_destroy();  // Elimina la sesión actual
-header("Location: inicio-de-sesión.php");  // Redirige al login
-exit();  // Finaliza la ejecución del script
-?>
